da0e5b6b130b50919d6a35608eb69b117e2dc39f
Alaaldin98 <alaaalhanene@gmail.com>
2022-03-09 12:27:14 +0300

    class17 done

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../header/Header";
+let NotFound = () => {
+    return(
+        <>
+        <Header />
+        <h1>Sorry! Not Found</h1>
+        </>
+    );
+};
+
+export default NotFound;
